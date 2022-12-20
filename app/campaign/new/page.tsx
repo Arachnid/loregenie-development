510ad2fd001d64d26b26100f0b563c6aab902c09
kai-commits <kaimeikle123@gmail.com>
2022-12-20 11:29:24 -0800

    LG-01: add test campaign writing

--- app/campaign/new/page.tsx
+++ app/campaign/new/page.tsx
@@ -1,3 +1,55 @@
+'use client';
+
+import { Button, TextField } from '@mui/material';
+import { Box } from '@mui/system';
+import {  useState } from 'react';
+
 export default function NewCampaignPage() {
-    return <div></div>;
+  const [campaignForm, setCampaignForm] = useState({
+    name: '',
+    description: '',
+  });
+
+  const onSubmit = async () => {
+    try {
+      await fetch('/api/setCampaign', {
+        method: 'POST',
+        body: JSON.stringify(campaignForm),
+      });
+    } catch (error) {
+      console.log('error submitting campaign: ', error);
+    }
+  };
+
+  return (
+    <div>
+      <Box
+        component='form'
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+      >
+        <TextField
+          label='name'
+          margin='normal'
+          value={campaignForm.name}
+          onChange={(e) =>
+            setCampaignForm({ ...campaignForm, name: e.target.value })
+          }
+        />
+        <TextField
+          label='description'
+          multiline
+          maxRows={4}
+          value={campaignForm.description}
+          onChange={(e) =>
+            setCampaignForm({ ...campaignForm, description: e.target.value })
+          }
+        />
+        <Button variant='contained' sx={{ met: 3 }} onClick={onSubmit}>
+          Submit Campaign
+        </Button>
+      </Box>
+    </div>
+  );
 }
