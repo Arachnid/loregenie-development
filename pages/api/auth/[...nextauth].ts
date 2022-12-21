2af258d406b87b184348459309159a4306bf125c
kai-commits <kaimeikle123@gmail.com>
2022-12-21 10:43:50 -0800

    LG-01: add support for twitch sso

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,22 @@
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
+import TwitchProvider from 'next-auth/providers/twitch';
 import { FirestoreAdapter } from '@lowfront/firebase-adapter';
-import { db } from '../../../lib/db';
+import { db } from '@/lib/db';
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID as string,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-        }),
-    ],
-    adapter: FirestoreAdapter(db),
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID as string,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: FirestoreAdapter(db),
 };
 
 export default NextAuth(authOptions);
