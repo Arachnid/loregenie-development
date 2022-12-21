2b906ed770901ac7ec2658cb9962a3f43d4e474f
kai-commits <kaimeikle123@gmail.com>
2022-12-20 19:05:06 -0800

    LG-01: move interfaces to separate file

---
+++ types.ts
@@ -0,0 +1,29 @@
+export interface Nav {
+  key: string;
+  children: Nav[];
+}
+
+export interface BaseCampaign {
+  name: string;
+  description: string;
+  readers: string[];
+  writers: string[];
+  admins: string[];
+}
+
+export interface ExtendedCampaign extends BaseCampaign  {
+  readonly id: string;
+  nav: Nav[];
+  owner: string;
+}
+
+export interface Location {
+  readonly id: string;
+  campaign: string;
+  name: string;
+  description: string;
+}
+
+export interface LocationMap {
+  [key: string]: Location,
+}
