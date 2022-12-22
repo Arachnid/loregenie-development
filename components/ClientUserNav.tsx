6ec807ff5ca5fdfca468da2585c55c7f18da1097
kai-commits <kaimeikle123@gmail.com>
2022-12-21 16:14:32 -0800

    LG-01: add home button

--- components/ClientUserNav.tsx
+++ components/ClientUserNav.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const ClientUserNav = ({children}: {children: JSX.Element}) => {
+  const router = useRouter();
   return (
     <>
       <Button onClick={() => signOut()}>Sign Out</Button>
+      <Button onClick={() => router.push('/')}>Home</Button>
       {children}
     </>
   );
