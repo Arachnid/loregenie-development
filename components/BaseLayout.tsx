43a348dbb05b49b5fb13f79e619bb6e6a12fcf10
kai-commits <kaimeikle123@gmail.com>
2022-12-29 18:13:40 -0800

    LG-01: fix mui loading unused modules

--- components/BaseLayout.tsx
+++ components/BaseLayout.tsx
@@ -1,27 +1,23 @@
 'use client';
 
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 import { ReactNode } from 'react';
 
 interface Props {
-    nav: ReactNode;
-    children: ReactNode;
+  nav: ReactNode;
+  children: ReactNode;
 }
 
 export default function BaseLayout({ nav, children }: Props) {
-    return (
-        <Grid container spacing={2}>
-        <Grid xs={4}>
-            <Box sx={{bgcolor: 'background.paper'}}>
-                <nav>
-                    {nav}
-                </nav>
-            </Box>
-        </Grid>
-        <Grid xs={8}>
-          {children}
-        </Grid>
-        </Grid>
-    )
+  return (
+    <Grid container spacing={2}>
+      <Grid xs={4}>
+        <Box sx={{ bgcolor: 'background.paper' }}>
+          <nav>{nav}</nav>
+        </Box>
+      </Grid>
+      <Grid xs={8}>{children}</Grid>
+    </Grid>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    modularizeImports: {
+      "@mui/material": {
+        transform: "@mui/material/{{member}}",
+      },
+      "@mui/icons-material": {
+        transform: "@mui/icons-material/{{member}}",
+      },
+    },
   },
 }
 
