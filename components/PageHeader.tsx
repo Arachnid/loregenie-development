9eb4600cbc09f240ada09b8e9677a1981124bdc3
kai-commits <kaimeikle123@gmail.com>
2023-02-03 17:24:08 -0800

    add permission rendering and parent dropdown search

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -44,15 +44,15 @@ const PageHeader = ({ entryData, permissions, worldID }: Props) => {
             <img className='w-8 h-8 rounded-full' src='/favicon.ico' alt='' />
             <img className='w-8 h-8 rounded-full' src='/favicon.ico' alt='' />
           </div>
-          <button
+          {permissions.includes('writer') && <button
             className='flex justify-center items-center py-3 px-4 gap-2 h-11 w-[100px] rounded-lg border-2 text-[16px] font-medium bg-white border-lore-beige-500 text-lore-blue-400 transition-all duration-300 ease-out hover:bg-lore-beige-400'
             onClick={() => setShowModal(!showModal)}
           >
             Sharing
-          </button>
-          <button className='flex justify-center items-center py-3 px-4 gap-2 h-11 w-[100px] rounded-lg border-2 text-[16px] font-medium border-lore-red-400 bg-lore-red-400 text-white transition-all duration-300 ease-out hover:bg-lore-red-500 hover:border-lore-red-500' onClick={() => onSave()}>
+          </button>}
+          {permissions.includes('writer') && <button className='flex justify-center items-center py-3 px-4 gap-2 h-11 w-[100px] rounded-lg border-2 text-[16px] font-medium border-lore-red-400 bg-lore-red-400 text-white transition-all duration-300 ease-out hover:bg-lore-red-500 hover:border-lore-red-500' onClick={() => onSave()}>
             Save
-          </button>
+          </button>}
         </div>
       </div>
       {showModal && (
--- components/entry/ParentDropDown.tsx
+++ components/entry/ParentDropDown.tsx
@@ -4,7 +4,13 @@ import { useOutsideClick } from '@/hooks/useOutsideClick';
 import { Category, Entry, EntryHierarchy, World } from '@/types';
 import { createEntryHierarchy } from '@/utils/createEntryHierarchy';
 import { getIcon } from '@/utils/getIcon';
-import { Dispatch, RefObject, SetStateAction } from 'react';
+import {
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
 import EntriesList from '../nav/EntriesList';
 
 type Props = {
@@ -22,6 +28,9 @@ const ParentDropDown = ({
   setEntryData,
   entryData,
 }: Props) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredSearch, setFilteredSearch] = useState<EntryHierarchy[]>([]);
+
   const parentDropDownRef: RefObject<HTMLDivElement> =
     useOutsideClick<HTMLDivElement>(() => setParentDropDownOpen(false));
 
@@ -47,14 +56,37 @@ const ParentDropDown = ({
     return result;
   };
 
+  useEffect(() => {
+    const filterSearch = getParents(entries).filter((parentEntry) => {
+      if (parentEntry.name.toLowerCase().includes(searchValue.toLowerCase())) {
+        return parentEntry;
+      }
+    });
+    setFilteredSearch(filterSearch);
+  }, [searchValue]);
+
   return (
     <div
       className='z-20 absolute flex flex-col w-full bg-white border-2 border-lore-beige-500 rounded-lg mt-12 min-w-max shadow-[0px_5px_10px_rgba(0,0,0,0.15)]'
       ref={parentDropDownRef}
     >
       <div className='flex items-center self-stretch justify-center px-4 py-3 border-b-2 border-lore-beige-500'>
-        <p className='font-light leading-5 grow'>Search</p>
-        <span className='text-[20px] material-icons'>search</span>
+        <input
+          className='leading-5 grow placeholder:text-black focus-visible:outline-none'
+          placeholder='Search'
+          value={searchValue}
+          onChange={(event) => setSearchValue(event?.target.value)}
+        />
+        {searchValue ? (
+          <span
+            className='text-[20px] cursor-pointer material-icons'
+            onClick={() => setSearchValue('')}
+          >
+            close
+          </span>
+        ) : (
+          <span className='text-[20px] cursor-default material-icons'>search</span>
+        )}
       </div>
       <div className='flex flex-col self-stretch p-2 overflow-y-scroll grow scrollbar-hide'>
         <div className='flex flex-col self-stretch grow text-lore-blue-400'>
@@ -70,7 +102,7 @@ const ParentDropDown = ({
             <p className='flex font-medium leading-5 grow'>{world.name}</p>
           </button>
           {/* <EntriesList entries={getParents(entries)} campaigns={[]} world={world} /> */}
-          {getParents(entries).map((parentEntry) => (
+          {filteredSearch.map((parentEntry) => (
             <button
               className='flex items-center self-stretch gap-2 p-2 transition-all duration-300 ease-out rounded-lg hover:bg-lore-beige-300'
               onClick={() => {
