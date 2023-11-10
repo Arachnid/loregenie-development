0c4263450ae5b7401eb7089a517b18b15e85ee3d
Nick Johnson <arachnid@notdot.net>
2023-11-10 15:00:12 +0000

    Update AI generation to use the latest models, and add campaign image generation.

--- pages/api/world/create.ts
+++ pages/api/world/create.ts
@@ -1,5 +1,5 @@
-import { Converter, db } from '@/lib/db';
-import { aiGenerate } from '@/lib/ai';
+import { Converter, db, storage } from '@/lib/db';
+import { aiGenerate, aiGenerateImage } from '@/lib/ai';
 import { WorldDB } from '@/types';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { contributorSanityCheck } from '@/utils/validation/contributorSanityCheck';
@@ -17,12 +17,23 @@ export default async function handler(
         'world',
         {
           name: 'Name for the setting',
-          description: 'A 2-4 paragraph description of the setting',
+          description: 'A detailed, 2-4 paragraph description of the setting',
+          imageDescription: 'A description of a header image that captures the setting, written in a way that someone who has never heard of the setting could paint a picture.'
         },
         [],
         prompt
       )
     );
+    const image = await aiGenerateImage(worldData.imageDescription);
+    const fileRef = storage.bucket().file('world.png');
+    await fileRef
+      .save(
+        Buffer.from(
+          image,
+          'base64'
+        )
+      );
+    worldData.image = fileRef.publicUrl();
   }
 
   try {
