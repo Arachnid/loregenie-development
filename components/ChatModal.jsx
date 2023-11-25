e7a793016f3c412b4a2984c503b833cb2a14017b
iyamujoshua <joshamu20@gmail.com>
2023-11-25 09:35:39 +0100

    redesigned the ui and made changes

--- components/ChatModal.jsx
+++ components/ChatModal.jsx
@@ -6,7 +6,7 @@ const UserChat = ({ userDp, prompt }) => {
     <div className="flex flex-col gap-2 items-end">
       <div className="flex items-center gap-[0.81rem]">
         <Image
-          src={userDp}
+          src="/userprofile.png"
           width={48}
           height={48}
           alt=""
@@ -24,7 +24,7 @@ const AiChat = ({ aiResults }) => {
     <div className="flex flex-col gap-[1.06rem] items-end">
       <div className="flex items-center gap-[0.81rem] self-start">
         <Image
-          src=""
+          src="/aiprofile.png"
           width={48}
           height={48}
           alt=""
@@ -43,10 +43,10 @@ export default function ChatModal({ onClose, inputValue, onSubmit }) {
   return (
     <>
       <div className="">
-        <div className="relative h-[90vh] max-w-[54rem] px-[2.81rem] bg-[#fff] rounded-[1.25rem] overflow-x-auto">
+        <div className="relative h-[90vh] max-w-[54rem] px-5 md:px-[2.81rem] bg-[#fff] rounded-[1.25rem] overflow-x-auto mx-4">
           <div
             onClick={onClose}
-            className="sticky top-0 w-full pt-[3.56rem] pb-2 flex items-center bg-white"
+            className="sticky top-0 w-full md:pt-[3.56rem] py-4 md:pb-2 flex items-center bg-white"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +61,7 @@ export default function ChatModal({ onClose, inputValue, onSubmit }) {
               />
             </svg>
           </div>
-          <div className="flex flex-col gap-10 ">
+          <div className="flex flex-col gap-3 ">
             <UserChat userDp="" prompt="Background" />
             <AiChat
               aiResults="My customers always come first, and my profits always come second I owe
@@ -69,7 +69,7 @@ export default function ChatModal({ onClose, inputValue, onSubmit }) {
         line of work I am so paranoid of the law that I often over-react to
         perceived threats"
             />
-            <UserChat userDp="" prompt="Background" />
+            <UserChat userDp="" prompt="Appearance" />
             <AiChat
               aiResults="My customers always come first, and my profits always come second I owe
         a debt of gratitude to a mysterious benefactor who set me up in this
@@ -77,38 +77,44 @@ export default function ChatModal({ onClose, inputValue, onSubmit }) {
         perceived threats"
             />
           </div>
-          <div className="sticky bottom-0 w-full p-2 mt-[5.19rem]">
-            <div className="px-3 py-2 flex items-center justify-between bg-[#4C7F8F] rounded-full">
-              <div className="w-[70%] flex justify-between bg-white rounded-full px-2 py-1">
+          <div className="sticky bottom-0 w-full p-2 mt-[5.19rem] bg-white md:pb-10 md:pt-5 pb-5 pt-3">
+            <div className="px-3 py-2 flex items-center justify-between bg-[#4C7F8F] rounded-full gap-3">
+              {/* <div className="w-[36rem] flex justify-between bg-white rounded-full px-2 py-1">
                 <input
                   type="text"
-                  placeholder="Type your message..."
+                  placeholder="Make it longer"
                   className="w-full flex-grow rounded-full px-2 py-1 mr-2 border border-transparent focus:outline-none"
                   value={inputValue}
                 />
                 <button className="p-4 bg-[#4C7F8F] rounded-full">
                   <Image src="/send-2.svg" width={24} height={24} alt="" />
                 </button>
+              </div> */}
+              <div className="w-full sm:w-[36rem] flex justify-between bg-white rounded-full px-2 py-1">
+                <input
+                  type="text"
+                  placeholder="Make it longer"
+                  className="w-full sm:flex-grow rounded-full px-2 py-1 mr-2 border border-transparent focus:outline-none"
+                  value={inputValue}
+                />
+                <button className="p-2 bg-[#4C7F8F] rounded-full">
+                  <Image src="/send-2.svg" width={24} height={24} alt="" />
+                </button>
               </div>
 
-              <div className="flex bg-[white] rounded-full p-2">
-              <Image src="/check.svg" width={20} height={20} alt="" />
+              <div className="flex items-center justify-center bg-[white] rounded-full px-4 py-1 w-[8.47919rem]">
+                <Image src="/check.svg" width={20} height={20} alt="" />
                 <button
                   onClick={onSubmit}
                   type="button"
-                  className="p-2 bg-[white] text-[#4C7F8F] rounded-[2rem] focus:outline-none focus:ring focus:border-blue-300"
+                  className="p-2 leading-none bg-[white] text-[#4C7F8F] rounded-[2rem] focus:outline-none "
                 >
-                  
                   Done
                 </button>
               </div>
             </div>
           </div>
         </div>
-
-        <button className="fixed bottom-0 right-0 p-4 m-4 bg-[#4C7F8F] rounded-full">
-        <span className='text-[20px] material-icons'>auto_fix_high</span>
-        </button>
       </div>
     </>
   );
